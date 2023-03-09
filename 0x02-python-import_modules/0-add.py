5b31cbbceb0a61a14f84e160ff2b320d3bf73b37
Habtemariam <117764997+habtemariamf@users.noreply.github.com>
2023-03-09 20:23:15 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-# 0_add.py
-# Brennan D Baraban <375@holbertonschool.com>
+def add(a, b):
+    """My addition function
 
-if __name__ == "__main__"
-    """Print the sum of 1 and 2"""
-    from add_0 import add
+    Args:
+        a: first integer
+        b: second integer
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
