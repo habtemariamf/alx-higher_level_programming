82cb704a4c7b8e96bf79123e8de6c84e1bbbc077
Habtemariam <117764997+habtemariamf@users.noreply.github.com>
2023-03-28 12:41:17 +0300

    Update 101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,68 +1,96 @@
 #!/usr/bin/python3
+""" Creates an empty class called Square
+"""
+
+
 class Square:
-    """Defines a square"""
+    """ Empty class with size private attribute
+    """
     def __init__(self, size=0, position=(0, 0)):
-        """Initialises the data"""
-        self.size = size
+        """
+                Instantiation with size
+        Args:
+            size: size of the square
+            position: postion of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
         self.position = position
 
     def area(self):
-        """Returns current square area"""
-        return self.__size**2
+        """
+        Returns the area of the square
+        """
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        """Getter method"""
+        """
+        size getter. Handle size errors
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter method"""
-        self.__size = value
-        if type(value) != int:
+        """
+        size setter. Set the size square
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def my_print(self):
-        """Prints the square"""
+        """
+        Print a square with the character # at position given
+        """
         if self.__size == 0:
             print()
-        else:
-            for y in range(self.__position[1]):
-                print()
-            for i in range(self.__size):
-                for x in range(self.__position[0]):
-                    print(' ', end='')
-                for j in range(self.__size):
-                    print('#', end='')
-                print()
+            return
+        for j in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            print(" " * self.__position[0], end="")
+            print("#" * self.__size)
 
     @property
     def position(self):
-        """Getter method"""
+        """
+        position setter. Set the position of square
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
-        """Setter method"""
-        if type(value) != tuple or len(value) != 2:
+        """
+        Handle position with errors
+        """
+        if type(value) != tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if any(type(i) != int for i in value) or any(j < 0 for j in value):
+        elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        elif isinstance(value[0], int) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif isinstance(value[1], int) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def __str__(self):
-        """Same print behaviour as my_print"""
-        s = ""
-        if not self.__size:
-            return s
-        for y in range(self.__position[1]):
-            s += '\n'
+        self.my_list = []
+        if self.__size == 0:
+            return ""
+        for j in range(self.__position[1]):
+            self.my_list.append("\n")
         for i in range(self.__size):
-            for x in range(self.__position[0]):
-                s += ' '
-            for j in range(self.__size):
-                s += '#'
-            s += '\n'
-        return s[: - 1]
+            self.my_list.append(" " * self.__position[0])
+            self.my_list.append("#" * self.__size)
+            if (i < self.__size - 1):
+                self.my_list.append("\n")
+        return "".join(self.my_list)
