66961433e5a47215a5c34edfbe93a780553a851a
Habtemariam <117764997+habtemariamf@users.noreply.github.com>
2023-07-04 16:18:48 +0300

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-Sends a request to the URL and displays the body of the response.
+Python script that takes in a URL, sends a request to the URL and displays the body of the response.
 """
 import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    try:
-        res = requests.get(argv[1])
-        res.raise_for_status()
-        print(res.text)
-    except:
-        print("Error code: {}".format(res.status_code))
+    resp = requests.get(argv[1])
+    code = resp.status_code
+    if code >= 400:
+        print("Error code: {}".format(code))
+    else:
+        print(resp.text)
